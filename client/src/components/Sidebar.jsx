5034d723005077bad11ec45c43bb7c15b257f800
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-11-07 16:52:15 +0000

    Add items and icons to the sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -34,6 +34,65 @@ import { useLocation, useNavigate} from "react-router-dom";
 import FlexBetween from './FlexBetween';
 import profile from "assets/profile.jpg"
 
+const navItems = [
+    {
+      text: "Dashboard",
+      icon: <HomeOutlined />,
+    },
+    {
+      text: "Client Facing",
+      icon: null,
+    },
+    {
+      text: "Products",
+      icon: <ShoppingCartOutlined />,
+    },
+    {
+      text: "Customers",
+      icon: <Groups2Outlined />,
+    },
+    {
+      text: "Transactions",
+      icon: <ReceiptLongOutlined />,
+    },
+    {
+      text: "Geography",
+      icon: <PublicOutlined />,
+    },
+    {
+      text: "Sales",
+      icon: null,
+    },
+    {
+      text: "Overview",
+      icon: <PointOfSaleOutlined />,
+    },
+    {
+      text: "Daily",
+      icon: <TodayOutlined />,
+    },
+    {
+      text: "Monthly",
+      icon: <CalendarMonthOutlined />,
+    },
+    {
+      text: "Breakdown",
+      icon: <PieChartOutlined />,
+    },
+    {
+      text: "Management",
+      icon: null,
+    },
+    {
+      text: "Admin",
+      icon: <AdminPanelSettingsOutlined />,
+    },
+    {
+      text: "Performance",
+      icon: <TrendingUpOutlined />,
+    },
+  ];
+
 const Sidebar = ({
     drawerWidth,
     isSidebarOpen,
@@ -51,7 +110,84 @@ const Sidebar = ({
 
     return (
         <Box component="nav">
+            {isSidebarOpen && (
+                <Drawer
+                    open={isSidebarOpen}
+                    onClose={()=>setIsSidebarOpen(false)}
+                    variant="persistent"
+                    anchor='left'
+                    sx={{
+                        width: drawerWidth,
+                        "& .MuiDrawer-paper": {
+                            color: theme.palette.secondary[200],
+                            backgroundColor: theme.palette.background.alt,
+                            boxSizing: "border-box",
+                            borderWidth: isNonMobile ? 0 : "2px",
+                            width: drawerWidth
+                        }
+                    }}
+                >
+                <Box width="100%">
+                    <box m="1.5rem 2rem 2rem 3rem">
+                        <FlexBetween color={theme.palette.secondary.main}>
+                            <Box display="flex" alignItems="center" gap="0.5rem">
+                                <Typography variant="h4" fontWeight="bold">
+                                    Newsletter Pro
+                                </Typography>
+                            </Box>
+                            {!isNonMobile && (
+                                <IconButton onClick={()=>setIsSidebarOpen(!isSidebarOpen)}>
+                                    <ChevronLeft />
+                                </IconButton>
+                            )}
+                        </FlexBetween>
+                    </box>
+                    <List>
+                        {navItems.map(({ text, icon }) => {
+                            if (!icon) {
+                                return (
+                                    <Typography key={text} sex={{ m: "2.25rem 0 1rem 3rem"}}>
+                                        {text}
+                                    </Typography>
+                                )
+                            }
+                            const lcText = text.toLowerCase();
 
+                            return (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton
+                                        onClick={() => { 
+                                            navigate(`/${lcText}`)
+                                            setActive(lcText);
+                                        }}
+                                        sx = {{
+                                            backgroundColor: active === lcText ? theme.palette.secondary[300] : "transparent",
+                                            color: active === lcText ? theme.palette.primary[600] : theme.palette.secondary[100],
+                                        }}
+                                    >
+                                        <ListItemIcon
+                                            sex={{
+                                                ml: "2rem",
+                                                color: active === lcText ? theme.palette.primary[600] : theme.palette.secondary[200],
+
+
+                                            }}
+                                            >
+                                                {icon}
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                        {active === lcText && (
+                                            <ChevronRightOutlined sx={{ml: "auto"}}/>
+                                        )}
+
+                                    </ListItemButton>
+                                </ListItem>
+                            )
+                        })}
+                    </List>
+                </Box>
+                </Drawer>
+            )}
         </Box>
     )
 
