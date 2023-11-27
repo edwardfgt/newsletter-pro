46bd0d21080a12818a8e29868a2d9c84e2ed5378
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-11-27 17:00:04 +0000

    Add additional mock data to db

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ import salesRoutes from "./routes/sales.js";
 //data imports
 import User from "./models/User.js";
 import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataProduct, dataProductStat } from "./data/index.js";
 
 /* config */
 dotenv.config();
@@ -41,6 +44,8 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
     /* ONLY ADD DATA ONE TIME */
+    //Product.insertMany(dataProduct);
+    //ProductStat.insertMany(dataProductStat);
     //User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
